d858ec99698efef7b1c0ed2e7528447e91e178f5
Amit Singh Dalal <amit.singh@magehost.com>
2021-04-11 15:41:39 +0530

    fix cart

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <nav class="navbar is-dark">
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item"><strong>Djackets</strong></router-link>
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -11,7 +11,7 @@
         </a>
       </div>
 
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
         <div class="navbar-end">
            <router-link to="/summer" class="navbar-item">Summer</router-link>
            <router-link to="/winter" class="navbar-item">Winter</router-link>
@@ -20,9 +20,11 @@
           <div class="buttons">
             <router-link to="/log-in" class="botton is-light">Log in</router-link>
 
-            <router-link to="/cart" class="button is-success"></router-link>
+
+            <router-link to="/cart" class="button is-success">
                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
                <span>Cart</span>
+              </router-link>
           </div>
         </div>
         </div>
